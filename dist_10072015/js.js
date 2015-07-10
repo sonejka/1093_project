0b6eb0788d0b5288ab58560d0a4bbe83af2c63d9
Kseniya Varabei <kseniya.varabei@gmail.com>
2015-07-10 17:28:52 +0300

    create new dist

---
+++ dist_10072015/js.js
@@ -0,0 +1,22 @@
+$(document).ready(function($) {
+
+
+    $('#tabs').tabulous({
+    	effect: 'scale'
+    });
+
+     $('#tabs2').tabulous({
+    	effect: 'slideLeft'
+    });
+
+     $('#tabs3').tabulous({
+    	effect: 'scaleUp'
+    });
+
+    $('#tabs4').tabulous({
+    	effect: 'flip'
+    });
+
+
+
+});
