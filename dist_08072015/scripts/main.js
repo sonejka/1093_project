30f0e2fc9e9f6cf40118fa3d2312c58cd15b70d7
Kseniya Varabei <kseniya.varabei@gmail.com>
2015-07-13 17:10:07 +0300

    fix bugs, add new blocks
    - fixed display of "Give your guests ...city." section
    - reset fonts (they downloads from google fonts)
    - added sign up section
    - added footer
    - started to work with step-4 block

--- dist_08072015/scripts/main.js
+++
@@ -1,9 +0,0 @@
-// jshint devel:true
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 1){
-    $('header').addClass("sticky");
-  }
-  else{
-    $('header').removeClass("sticky");
-  }
-});
