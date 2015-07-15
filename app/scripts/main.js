4af86adfe0c50767f085d5ddd261be89985e2d95
Kseniya Varabei <kseniya.varabei@gmail.com>
2015-07-15 15:44:25 +0300

    updated droppin behavior

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,10 @@
 // jshint devel:true
 $(window).scroll(function() {
   if ($(this).scrollTop() > 1){
-    $('header').addClass("sticky");
+    $('.animated-header').addClass("sticky");
   }
   else{
-    $('header').removeClass("sticky");
+    $('.animated-header').removeClass("sticky");
   }
 });
 
@@ -15,7 +15,6 @@ window.onload = function() {
       $(".top-nav").toggleClass("opened");
       $(".login-nav").toggleClass("opened");
       $(".overlay").toggleClass("active");
-      //$(".menu-btn").toggleClass("opened");
     });
   });
 
@@ -23,7 +22,39 @@ window.onload = function() {
     $(".top-nav").removeClass("opened");
     $(".login-nav").removeClass("opened");
     $(".overlay").removeClass("active");
-    //$(".menu-btn").removeClass("opened");
   });
+
+  //$( ".preference-1" ).hover(function() {
+  //  $(".drop-pin-1").toggleClass("opened");
+  //});
+  //$( ".preference-2" ).hover(function() {
+  //  $(".drop-pin-2").toggleClass("opened");
+  //});
+  //$( ".preference-3" ).hover(function() {
+  //  $(".drop-pin-3").toggleClass("opened");
+  //});
+  //$( ".preference-4" ).hover(function() {
+  //  $(".drop-pin-4").toggleClass("opened");
+  //});
+  //$( ".preference-5" ).hover(function() {
+  //  $(".drop-pin-5").toggleClass("opened");
+  //});
+
+  $( ".preference-1" ).hover(function() {
+    $(".preferences-list").toggleClass("drop-pin-1-bg");
+  });
+  $( ".preference-2" ).hover(function() {
+    $(".preferences-list").toggleClass("drop-pin-2-bg");
+  });
+  $( ".preference-3" ).hover(function() {
+    $(".preferences-list").toggleClass("drop-pin-3-bg");
+  });
+  $( ".preference-4" ).hover(function() {
+    $(".preferences-list").toggleClass("drop-pin-4-bg");
+  });
+  $( ".preference-5" ).hover(function() {
+    $(".preferences-list").toggleClass("drop-pin-5-bg");
+  });
+
 
 }
